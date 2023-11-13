890d1c959316fb5e32b9be455aa9bfdaccff3cd5
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2023-11-13 22:21:26 +0700

    menambah perulangan

--- PemesananTiketKereta.java
+++ PemesananTiketKereta.java
@@ -3,26 +3,35 @@ import java.util.Scanner;
 public class PemesananTiketKereta {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int harga, jumlah, jml=0;
-        String kotaTujuan, kelas;
+        int harga, jml = 0;
+        String kotaTujuan, kelas, nama;
         double bayar, totalharga, kembalian;
-
+        char jawab;
 
         System.out.println("Selamat Datang di Stasiun Malang");
         System.out.println("Kemana Tujuan Anda?");
-
+        
+        // input kota tujuan dan kelas kereta
         System.out.println("Masukkan Pilihan Anda");
         System.out.print("Kota Tujuan : ");
         kotaTujuan = input.next();
         System.out.print("Kelas Kereta : ");
         kelas = input.next();
-       
-        // bersarang / nested if
+
+        // perulangan
+        do {
+            System.out.println("Masukkan nama pelanggan : ");
+            nama = input.next();
+            jml++;
+            System.out.println("Apakah anda ingin menambah pelanggan lain (Y/T)? ");
+            jawab = input.next().charAt(0);
+        } while (jawab == 'y' || jawab == 'Y');
+        System.out.println("Total Pelanggan adalah : " + jml);
+
         System.out.print("Harga Tiket : ");
         harga = input.nextInt();
-        System.out.println("Masukkan Jumlah tiket : ");
-        jml = input.nextInt();
-
+       
+        // bersarang / nested if
         if (kotaTujuan.equals("sby")) {
             if (jml > 5) {
                 totalharga = (harga * jml) - (harga * jml * 0.05);
@@ -40,18 +49,8 @@ public class PemesananTiketKereta {
                 System.out.println("Total Harga : " + totalharga);
                 System.out.print("Masukkan nominal pembayaran anda : ");
                 bayar = input.nextDouble();
-                  if (bayar > totalharga) {
-                kembalian = bayar - totalharga;
-                System.out.println("Kembalian anda adalah = " + kembalian);
-            } else if (bayar == totalharga) {
-                System.out.println("Uang anda pas");
-            }
             }  
         
         }
     }
 }
-
-
-
-
