cd7eb75415804b2ca2b611d09940811779362b28
septyandini921 <septyandini921@gmail.com>
2023-11-13 22:30:20 +0700

    Array kereta & gerbong

--- PemesananTiketKereta.java
+++ PemesananTiketKereta.java
@@ -3,25 +3,43 @@ import java.util.Scanner;
 public class PemesananTiketKereta {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String []kotaTujuan = {"Surabaya", "Jakarta"};
-        int[] harga = {20000, 50000};
+        int[][] nominalHarga = {
+            {36000, 46000}, //sby
+            {200000, 300000} //jkt
+        };
         int a, pilihan, jumlah, jml = 0;
-        String kelas, nama;
+        String nama;
+        //String kelas;
         double bayar, totalharga, kembalian;
         char jawab;
 
         System.out.println("Selamat Datang di Stasiun Malang");
-        System.out.println("Kemana Tujuan Anda?");
+        System.out.println("Silahkan Memilih Tujuan Anda?");
+        //memilih kereta
+        System.out.println("1. Surabaya, KA Jayabaya");
+        System.out.println("2. Jakarta, KA Brawijaya");
+        System.out.print("Pilihan anda: ");
+        pilihan=input.nextInt();
 
-        for (a=0; a < harga.length; a++){
-             System.out.println(a+ ". " + kotaTujuan[a] + " Harganya " + harga[a]);
-             }
+        int kotaTujuan = pilihan - 1;
+
+       //memilih gerbong
+       System.out.println("Silahkan memilih gerbong kereta: ");
+       System.out.println("1. Gerbong Ekonomi");
+       System.out.println("2. Gerbong Eksekutif");
+    System.out.print("Pilihan anda: ");
+       int gerbongKereta = input.nextInt();
+
         
         // input kota tujuan dan kelas kereta
-        System.out.print("Masukkan Pilihan Anda: ");
-        pilihan=input.nextInt();
-        System.out.println("Kota Tujuan : " + kotaTujuan[pilihan]);
-        System.out.println("Harga Tiket : " + harga[pilihan]);
+        // System.out.print("Masukkan Pilihan Anda: ");
+        
+        // System.out.println("Kota Tujuan : " + kotaTujuan[pilihan]);
+        // System.out.println("Harga Tiket : " + harga[pilihan]);
+
+        //harga tiket
+        int hargaTiket = nominalHarga[kotaTujuan][gerbongKereta - 1];
+        System.out.println("Harga tiket: " + hargaTiket);
 
         // perulangan
         do {
@@ -34,9 +52,9 @@ public class PemesananTiketKereta {
         System.out.println("Total Pelanggan adalah : " + jml);
 
         // bersarang / nested if
-        if (kotaTujuan[pilihan].equals("Surabaya")) {
+        if (kotaTujuan==1) {
             if (jml > 5) {
-                totalharga = (harga[pilihan] * jml) - (harga[pilihan] * jml * 0.05);
+                totalharga = (hargaTiket * jml) - (hargaTiket * jml * 0.05);
                 System.out.println("Total Harga : " + totalharga);
                 System.out.print("Masukkan nominal pembayaran anda : ");
                 bayar = input.nextDouble();
@@ -49,7 +67,7 @@ public class PemesananTiketKereta {
             } 
         
              }else {
-                totalharga = harga[pilihan] * jml;
+                totalharga = hargaTiket * jml;
                 System.out.println("Total Harga : " + totalharga);
                 System.out.print("Masukkan nominal pembayaran anda : ");
                 bayar = input.nextDouble();
